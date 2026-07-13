5b7c544606b796e77db291c82696fb66c39364eb
leadoss <leademoww@gmail.com>
2026-07-13 14:12:22 +0300

    Update: src/app/page.tsx,

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,7 +10,6 @@ export default function HomePage() {
   return (
     <>
       <HeroSection />
-      <AsSeenIn />
       <FlavorsSection />
       <SocialProofStrip />
       <IngredientsSection />
